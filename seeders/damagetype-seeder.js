2d2fbe861e4d3af7c5815ddffe2f72ea463b6a1b
JoaoGAR <jgarchanjo@gmail.com>
2026-02-28 17:39:26 +0000

    damage types

--- seeders/damagetype-seeder.js
+++ seeders/damagetype-seeder.js
@@ -5,24 +5,20 @@ module.exports = {
         const now = new Date();
 
         await queryInterface.bulkInsert('DamageTypes', [
-            { name: 'PHYSICAL', icon: 'icons/damage/physical.jfif', createdAt: now, updatedAt: now },
-            { name: 'FIRE', icon: 'icons/damage/fire.jfif', createdAt: now, updatedAt: now },
-            { name: 'COLD', icon: 'icons/damage/cold.jfif', createdAt: now, updatedAt: now },
-            { name: 'LIGHTNING', icon: 'icons/damage/lightning.jfif', createdAt: now, updatedAt: now },
-            { name: 'ACID', icon: 'icons/damage/acid.jfif', createdAt: now, updatedAt: now },
-            { name: 'POISON', icon: 'icons/damage/poison.jfif', createdAt: now, updatedAt: now },
-            { name: 'NECROTIC', icon: 'icons/damage/necrotic.jfif', createdAt: now, updatedAt: now },
-            { name: 'RADIANT', icon: 'icons/damage/radiant.jfif', createdAt: now, updatedAt: now },
-            { name: 'PSYCHIC', icon: 'icons/damage/psychic.jfif', createdAt: now, updatedAt: now },
-            { name: 'FORCE', icon: 'icons/damage/force.jfif', createdAt: now, updatedAt: now },
-            { name: 'THUNDER', icon: 'icons/damage/thunder.jfif', createdAt: now, updatedAt: now },
-            { name: 'HOLY', icon: 'icons/damage/holy.jfif', createdAt: now, updatedAt: now },
+            { name: 'PHYSICAL', icon: 'icons/damage/physical.png', createdAt: now, updatedAt: now },
+            { name: 'FIRE', icon: 'icons/damage/fire.png', createdAt: now, updatedAt: now },
+            { name: 'COLD', icon: 'icons/damage/cold.png', createdAt: now, updatedAt: now },
+            { name: 'LIGHTNING', icon: 'icons/damage/lightning.png', createdAt: now, updatedAt: now },
+            { name: 'POISON', icon: 'icons/damage/poison.png', createdAt: now, updatedAt: now },
+            { name: 'PSYCHIC', icon: 'icons/damage/psychic.png', createdAt: now, updatedAt: now },
+            { name: 'HOLY', icon: 'icons/damage/holy.png', createdAt: now, updatedAt: now },
+            { name: 'DARK', icon: 'icons/damage/dark.png', createdAt: now, updatedAt: now }
         ]);
     },
 
     async down(queryInterface, Sequelize) {
         await queryInterface.bulkDelete('DamageTypes', {
-            name: ['PHYSICAL', 'FIRE', 'COLD', 'LIGHTNING', 'ACID', 'POISON', 'NECROTIC', 'RADIANT', 'PSYCHIC', 'FORCE', 'THUNDER', 'HOLY']
+            name: ['PHYSICAL', 'FIRE', 'COLD', 'LIGHTNING', 'POISON', 'NECROTIC', 'RADIANT', 'PSYCHIC', 'HOLY']
         });
     }
 };
